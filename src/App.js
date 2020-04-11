8ab96684ed8d871a6fb870d7fc08b86060663dfd
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2020-04-11 10:57:22 +0200

    Added HTTP Request 'get' Data in state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './scss/App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <h1></h1>
-    </div>
-  );
+import Table from './components/body/table.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+    };
+  }
+
+  //Get data after the component mounts
+  componentDidMount() {
+    this.getData();
+  }
+
+  //Data from HTTP request
+  getData() {
+    let URL = 'http://localhost:5000';
+    let xhr = new XMLHttpRequest();
+
+    //Callback when Server responds
+    xhr.addEventListener('load', () => {
+      console.log('Server responds' + xhr.responseText);
+      alert('Server responds' + xhr.responseText);
+      this.setState({ data: xhr.responseText });
+    });
+    xhr.open('GET', URL);
+    xhr.send();
+  }
+
+  render() {
+    let { content, time } = this.state.data;
+    return (
+      <div className="App">
+        <div className="container">
+          <Table content={content} time={time} />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/body/table.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Table extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Test</h1>
+      </div>
+    );
+  }
+}
+
+export default Table;
