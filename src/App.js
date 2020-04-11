57837b5cde68a166396202f402495fb43898850b
Ruslan Mochulskyy <mochulskyy.ruslan@gmail.com>
2020-04-11 15:38:36 +0200

    Added table which is filled with json values from an array
    Data is now retreived from the backend
    
    The QRTable is only displayed after retreiving the values from the backend

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import './scss/App.scss';
 
-import Table from './components/body/table.js';
-
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [],
+      dateRetrieved: false,
+      data: {},
     };
   }
 
@@ -18,29 +17,61 @@ class App extends Component {
 
   //Data from HTTP request
   getData() {
-    let URL = 'http://localhost:5000';
+    let URL = 'http://localhost:5000/data';
     let xhr = new XMLHttpRequest();
 
     //Callback when Server responds
     xhr.addEventListener('load', () => {
       console.log('Server responds' + xhr.responseText);
-      alert('Server responds' + xhr.responseText);
-      this.setState({ data: xhr.responseText });
+      this.setState({
+        data: JSON.parse(xhr.responseText),
+        dateRetrieved: true,
+      });
     });
     xhr.open('GET', URL);
     xhr.send();
   }
 
   render() {
-    let { content, time } = this.state.data;
     return (
       <div className="App">
         <div className="container">
-          <Table content={content} time={time} />
+          <QRTable
+            dataRetrieved={this.state.dateRetrieved}
+            data={this.state.data}
+          />
         </div>
       </div>
     );
   }
 }
-
 export default App;
+
+function QRTable(props) {
+  const dataRetrieved = props.dataRetrieved;
+
+  if (dataRetrieved) {
+    let data = props.data.qr;
+
+    //Return data as HTML
+    let table_items = data.map((content, index) => {
+      return (
+        <tr key={index}>
+          <td>{content.content}</td>
+        </tr>
+      );
+    });
+
+    return (
+      <table className="qr-table">
+        <thead>
+          <tr>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>{table_items}</tbody>
+      </table>
+    );
+  }
+  return <h1>No data received yet!</h1>;
+}
